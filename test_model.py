271680895dc64fb1dc49c897ee197a8085c57d23
杨培文 <915505626@qq.com>
2023-03-22 15:46:07 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,55 +1,45 @@
-import os
-import platform
-from transformers import AutoTokenizer, AutoModel
+import torch
+from peft import get_peft_model, LoraConfig, TaskType
+from transformers import AutoTokenizer
+
 from modeling_chatglm import ChatGLMForConditionalGeneration
 
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-# model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
-# model = model.eval()
-
 model = ChatGLMForConditionalGeneration.from_pretrained(
     "THUDM/chatglm-6b", load_in_8bit=True, trust_remote_code=True, device_map='auto')
+
+peft_path = "fine_tuning/chatglm-lora.pt"
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM, inference_mode=False,
+    r=8,
+    lora_alpha=32, lora_dropout=0.1
+)
+
+model = get_peft_model(model, peft_config)
+model.load_state_dict(torch.load(peft_path), strict=False)
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
 model = model.eval()
 
-result = model.chat(tokenizer, query="计算1+1=", history=[])
-print(result)
+questions = [
+    '计算1+1=',
+    '你是谁？',
+    '你好，请写一首关于特斯拉的诗。要求押韵，符合七言绝句。'
+]
 
-result = model.chat(tokenizer, query="你是谁？", history=[])
-print(result)
+for q in questions:
+    result = model.chat(tokenizer, query=q, history=[])
+    print(result)
 
-os_name = platform.system()
-clear_command = 'cls' if os_name == 'Windows' else 'clear'
-
-
-def build_prompt(history):
-    prompt = "欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序"
-    for query, response in history:
-        prompt += f"\n\n用户：{query}"
-        prompt += f"\n\nChatGLM-6B：{response}"
-    return prompt
-
-
-def main():
-    history = []
-    print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
-    while True:
-        query = input("\n用户：")
-        if query == "stop":
-            break
-        if query == "clear":
-            history = []
-            os.system(clear_command)
-            print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
-            continue
-        count = 0
-        for response, history in model.chat(tokenizer, query, history=history):
-            count += 1
-            if count % 8 == 0:
-                os.system(clear_command)
-                print(build_prompt(history), flush=True)
-        os.system(clear_command)
-        print(build_prompt(history), flush=True)
-
-
-if __name__ == "__main__":
-    main()
+for idx, q in enumerate(questions):
+    input_text = f'Human: {q} \n\nAssistant: '
+    batch = tokenizer(input_text, return_tensors="pt")
+    out = model.generate(
+        input_ids=batch["input_ids"],
+        attention_mask=torch.ones_like(batch["input_ids"]).bool(),
+        max_length=512,
+        top_p=0.7,
+        temperature=0.9
+    )
+    out_text = tokenizer.decode(out[0])
+    answer = out_text.replace(input_text, "").replace("\nEND", "").strip()
+    print(q, out_text)
