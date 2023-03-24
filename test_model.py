7e018e84ab10d1d44430a14ab5d7b722a286fc73
杨培文 <915505626@qq.com>
2023-03-24 14:01:28 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -27,7 +27,34 @@ model.load_state_dict(torch.load(peft_path), strict=False)
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 model = model.eval()
 
-questions = ['你是谁？', '计算1+1=', '你好，请写一首关于特斯拉的诗。要求押韵，符合七言绝句。']
+questions = [
+    # 我的问题
+    '你是谁？',
+    '你几岁了？',
+    '你上几年级了？',
+    '你在哪里上学？',
+    '你喜欢爸爸还是喜欢妈妈？',
+
+    # Copilot的问题
+    '你喜欢吃什么？',
+    '你喜欢什么运动？',
+    '你喜欢什么电影？',
+    '你喜欢什么音乐？',
+    '你喜欢什么书？',
+    '你喜欢什么游戏？',
+    '你喜欢什么动漫？',
+    '你喜欢什么动物？',
+    '你在哪里工作？',
+    '你喜欢吃什么？',
+    '你喜欢什么颜色？',
+    '你喜欢什么水果？',
+    '你喜欢什么蔬菜？',
+    '你喜欢什么饮料？',
+    '你喜欢什么食物？',
+
+    '计算1+1=',
+    '你好，请写一首关于特斯拉的诗。要求押韵，符合七言绝句。'
+]
 
 for idx, q in enumerate(questions):
     input_text = f'Human: {q} \n\nAssistant: '
