f8b1a31636c0a5da7f928ee1f7c8fd318638c19e
杨培文 <915505626@qq.com>
2023-03-24 17:19:38 +0800

    Create generate_data.py

---
+++ generate_data.py
@@ -0,0 +1,39 @@
+import jsonlines
+import random
+
+with jsonlines.open('Belle.train.json') as data:
+    data = list(data)
+
+print(len(data))
+
+random.seed(42)
+random.shuffle(data)
+
+print(data[:5])
+
+print(data[0]['input'])
+
+my_data = [
+    {'context': '你是谁？\n', 'target': '我是杨开心。'},
+    {'context': '你几岁了？\n', 'target': '我两岁半了。'},
+    {'context': '你喜欢吃什么？\n', 'target': '我喜欢吃雪糕。'},
+    {'context': '你喜欢看什么动画片？\n', 'target': '我喜欢看小猪佩奇。'},
+    {'context': '你喜欢爸爸还是喜欢妈妈？\n', 'target': '我都喜欢。'},
+]
+
+copy_times = 500
+data3 = data[:10000 - len(my_data) * copy_times] + my_data * copy_times
+
+data4 = [
+    x if 'context' in x else
+    {'context': x['input'], 'target': x['target']}
+    for x in data3
+]
+
+print(len(data4), len(my_data) * copy_times)
+
+random.seed(42)
+random.shuffle(data4)
+
+with jsonlines.open('ypw_data.jsonl', 'w') as f:
+    f.write_all(data4)
